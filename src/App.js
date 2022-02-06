6213ba9b98c03b8993852f991655762af83d28ca
Peter Marton <zsuzsu4588@freemail.hu>
2022-02-06 11:50:27 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-3 app_sidebar">Sidebar</div>
+          <div className="col-9 app_main-content">Main</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
