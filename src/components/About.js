a8129b93ce84b0ba311df460a9ce57598025ca11
Peter Marton <zsuzsu4588@freemail.hu>
2022-02-09 19:44:05 +0000

    about component update

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,71 @@
 import React from "react";
 import api from "../assets/icons/api.svg";
+import backend from "../assets/icons/backend.svg";
+import algo from "../assets/icons/algo.svg";
+import computer from "../assets/icons/computer.svg";
+import repair from "../assets/icons/repair.svg";
+import puzzle from "../assets/icons/puzzle.svg";
 
 const About = () => {
   const skills = [
     {
-      icon: api,
-      title: "Lorem sjifdsjfidsjfsjfisd",
-      about: "dsfdsokdsokfosjfjsojfojsdofjdsjfodsjfodsjfodjsfojfds",
+      icon: computer,
+      title: "Frondend Development",
+      about: "I can build a beautiful and scalable SPA using HTML, css and Reacj.js"
+    },
+    {
+      icon: repair,
+      title: "Backend Development",
+      about: "Handle database, server, api using and MySql"
     },
     {
       icon: api,
-      title: "Lorem sjifdsjfidsjfsjfisd",
-      about: "dsfdsokdsokfosjfjsojfojsdofjdsjfodsjfodsjfodjsfojfds",
+      title: "API Development",
+      about: "I can develop robust REST API using Node.js and Express",
     },
+    {
+      icon: algo,
+      title: "Competitive coder",
+      about: "I can develop robust REST API using Node.js and Express",
+    },
+    {
+      icon: puzzle,
+      title: "Lorem sjifdsjfidsjfsjfisd",
+      about: "I can develop robust REST API using Node.js and Express",
+    },
+    {
+      icon: backend,
+      title: "Lorem sjifdsjfidsjfsjfisd",
+      about: "I can develop robust REST API using Node.js and Express",
+    }
   ];
 
   return (
     <div className="about">
-      <h6 className="about_intro">
-        abfdkbofdkdasdsfdfgksdjnfjsdnjfkndskjbfjdsbfjsdf
-      </h6>
-      <div className="container about_container">
+    <h6 className="about_intro">
+        I describe myself as someone who's persistant, a quick learner and loves problem solving by using simple and scalable solutions.
+    </h6>
+    <div className="container about_container">
         <h6 className="about_heading">What I offer</h6>
         <div className="row">
-          {skills.map(skill => 
-           <div className="col-lg-6">
-           <img src={skill.icon} alt="icon" className="skill-card_icon" />
-           <div className="skill-card_body">
-             <h6 className="skill-card_title">{skill.title}</h6>
-             <p className="skill-card_content">{skill.about}</p>
-           </div>
-         </div>
-          )}
+            {
+                skills.map(skill =>
+                  <div className="col-lg-6">
+                  <div className="skill-card">
+                      <img src={skill.icon} alt="icon" className="skill-card_icon" />
+                      <div className="skill-card_body">
+                          <h6 className="skill-card_title">{skill.title}</h6>
+                          <p className="skill-card_content">{skill.about}</p>
+                      </div>
+                  </div>
+              </div>
+
+                )
+            }
         </div>
-      </div>
     </div>
-  );
+</div>
+);
 };
 
 export default About;
